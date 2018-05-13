30014ab0c1f12c29e8428149c265b00d4c851390
Tomas Lycken <tlycken@users.noreply.github.com>
2018-05-13 20:53:59 +0200

    Add page about the sites

--- app/src/pages/pages.jsx
+++ app/src/pages/pages.jsx
@@ -1,6 +1,7 @@
 import Home from './home/home.jsx'
 import WeddingWeekend from './wedding-weekend/wedding-weekend.jsx'
 import FindYourWay from './find-your-way/find-your-way.jsx'
+import TheSites from './the-sites/the-sites.jsx'
 import Accommodation from './accommodation/accommodation.jsx'
 import Rsvp from './rsvp/rsvp.jsx'
 export { NotFound } from './errors/errors.jsx'
@@ -10,7 +11,8 @@ export { default as Thanks } from './rsvp/thanks.jsx'
 export const menuItems = [
   { url: '/', text: 'Hem', component: Home, exact: true },
   { url: '/brollopshelgen', text: 'Bröllopshelgen', component: WeddingWeekend },
-  { url: '/hitta', text: 'Hitta rätt', component: FindYourWay },
+  { url: '/platserna', text: 'Platserna', component: TheSites },
   { url: '/overnattning', text: 'Övernattning', component: Accommodation },
+  { url: '/hitta', text: 'Hitta rätt', component: FindYourWay },
   { url: '/osa', text: 'O.S.A.', component: Rsvp }
 ]
---
+++ app/src/pages/the-sites/the-sites.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h2>Sjöviken</h2>
+    <p>
+      I Sörmlands skogar, vid stranden till Gålsjön, ligger ett stort rött hus
+      och några uthus - Sjöviken. Det brukade vara ett kollo för barn på
+      sommaräventyr, något dess storlek vittnar om.
+    </p>
+  </div>
+)
